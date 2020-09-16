2bdf094ab8d9aa59d18256640d4ae1c46aceaa0d
yasuo <sasaki6503@gmail.com>
2020-09-16 14:45:01 +0900

    Initial sharing of project

---
+++ ObjectPropertyEx.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class ObjectPropertyEx here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ObjectPropertyEx
+{
+    public static void main(String[] args)
+    {
+        Rectangle rec = new Rectangle(60, 20);
+        Rectangle rec2 = new Rectangle(40, 30);
+        
+        rec.equals(rec2);
+        
+        System.out.println();
+    }
+    
+    
+}
---
+++ Rectangle.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Rectangle here.
+ *
+ * @author (2017210105 김민수)
+ * @version (20200914)
+ */
+public class Rectangle
+{
+    // instance variables - replace the example below with your own
+    private int width;
+    private int height;
+    
+
+    /**
+     * Constructor for objects of class Rectangle
+     */
+    public Rectangle(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+     }
+
+    public boolean equals(Object obj)
+    {
+        area = 
+        if(area == )
+            return true;
+        else
+            return false;
+    }
+}
+ 
